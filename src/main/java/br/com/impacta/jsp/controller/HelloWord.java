03ae717a56fb95565049f2832ef0ebcb79ae9eeb
alex <alexmbcdo@gmail.com>
2022-06-02 16:00:18 -0300

    add configurações ao projeto

--- src/main/java/br/com/impacta/jsp/controller/HelloWord.java
+++ src/main/java/br/com/impacta/jsp/controller/HelloWord.java
@@ -1,33 +1,15 @@
 package br.com.impacta.jsp.controller;
 
-import br.com.impacta.jsp.model.Usuarios;
-import br.com.impacta.jsp.service.UsuarioService;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@RestController
-@RequestMapping("/hello")
-@RequiredArgsConstructor
+@Controller
 public class HelloWord {
-
-  private final UsuarioService usuarioService;
-
-  @GetMapping
+  @RequestMapping("/hello")
   public String getHello() {
-    log.info("acessou metodo get url hello");
-    return "HelloWord";
+    log.info("acessou metodo get url hello: ");
+    return "hello";
   }
-
-  @PostMapping
-  public Usuarios postUsuario(@RequestBody Usuarios usuarios) {
-    log.info("acessou metodo post url hello");
-    return usuarioService.salvar(usuarios);
-  }
-
 }
